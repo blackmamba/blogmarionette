572811a8cf99ace721b040ae79904b71a0bc74fd
Hitesh Ubharani <hubharani@gmail.com>
2015-08-27 14:00:45 -0700

    added empty itemview

--- client/js/views/emptyview.js
+++ client/js/views/emptyview.js
@@ -2,12 +2,12 @@ define(function(require) {
 	'use strict'
 
 	var Marionette = require('marionette'),
-	template = require('tpl!templates/detailview.tpl');
+	template = require('tpl!templates/emptyitemview.tpl');
     
     return Marionette.ItemView.extend({
         template: template,
         tag: 'div',
-        className: 'detail',
+        className: 'item',
     });
 
 });
